f07d3e9bde09bd95e36d6f7f0baa844fb214902a
Roy Segall <roy.segall@gizra.com>
2015-07-07 21:06:27 +0300

    Add the design.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
 Nuntiis.controller('FrontPageCtrl', function($scope) {
-  $scope.foo = 'bar';
+  $scope.messages = {
+    0: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    1: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    }
+  };
 });
