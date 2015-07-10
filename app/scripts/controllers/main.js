35c3961ec3a67ac38a5517cbab5c035924a9164b
Roy Segall <roy.segall@gizra.com>
2015-07-10 16:57:04 +0300

    Done with the base layout.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,11 +4,89 @@ Nuntiis.controller('FrontPageCtrl', function($scope) {
   $scope.messages = {
     0: {
       name: 'RoySegall - Roy Segall',
+      picture: 'images/avatars/divyia.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    2: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'images/avatars/divyia.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    3: {
+      name: 'RoySegall - Roy Segall',
       picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
       text: 'This is a test',
       date: '7/7/14 20:30'
     },
-    1: {
+    4: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    5: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    6: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    7: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    8: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    9: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    10: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    11: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    12: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    13: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    14: {
+      name: 'RoySegall - Roy Segall',
+      picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
+      text: 'This is a test',
+      date: '7/7/14 20:30'
+    },
+    15: {
       name: 'RoySegall - Roy Segall',
       picture: 'http://41.media.tumblr.com/960a5bae6a352b5868cef43c47c4d662/tumblr_nokmh491yx1trvouao1_1280.jpg',
       text: 'This is a test',
